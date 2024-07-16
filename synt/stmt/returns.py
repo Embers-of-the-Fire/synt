5bfa9d21f09c0a7d6f741b4ed055da52a5f6e91a
Embers-of-the-Fire <stellarishs@163.com>
2024-07-16 10:18:22 +0800

    add: add `return` statement

---
+++ synt/stmt/returns.py
@@ -0,0 +1,42 @@
+from synt.expr.expr import Expression, IntoExpression
+from synt.stmt.stmt import Statement
+
+
+class Return(Statement):
+    """The `return` statement.
+
+    Examples:
+        ```python
+        return_stmt = return_(litint(42))
+        assert return_stmt.into_code() == "return 42"
+        return_stmt = ret()
+        assert return_stmt.into_code() == "return"
+        ```
+
+    References:
+        [`Returns`](https://docs.python.org/3/library/ast.html#ast.Return).
+    """
+
+    expression: Expression | None
+    """The value to return from the function."""
+
+    def __init__(self, expression: IntoExpression | None = None):
+        """Initialize the return statement.
+
+        Args:
+            expression: The value to return from the function.
+        """
+        if expression:
+            self.expression = expression.into_expression()
+        else:
+            self.expression = None
+
+    def indented(self, indent_width: int, indent_atom: str) -> str:
+        if self.expression:
+            return f"{indent_atom * indent_width}return {self.expression.into_code()}"
+        else:
+            return f"{indent_atom * indent_width}return"
+
+
+return_ = ret = Return
+"""Alias [`Return`][synt.stmt.returns.Return]."""
